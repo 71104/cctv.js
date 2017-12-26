83ffc48276d479c7d4d9bf0025821f1a1a1ca10b
71104 <a71104@gmail.com>
2017-12-26 11:04:14 +0000

    Synchronous workflow

--- static/js/call.js
+++ static/js/call.js
@@ -1,21 +1,31 @@
-function doCall(index, socket) {
-  const pc = new RTCPeerConnection();
-
-  socket.on('call', function (index, sdp) {
-    pc.setRemoteDescription(sdp).catch(function (error) {
-      console.error(error);
-    });
-  }).emit('central');
-
-  pc.createOffer().then(function (sdp) {
-    pc.setLocalDescription(sdp);
-    socket.emit('call', index, sdp);
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
 $(function () {
+  const ready = async function (pc, index, answer) {
+    try {
+      await pc.setRemoteDescription(answer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const call = async function (index, socket) {
+    const pc = new RTCPeerConnection();
+    pc.onaddstream = function (event) {
+      $('video#video').prop('srcObject', event.streams[0]);
+    };
+
+    socket.on('call', function (index, answer) {
+      ready(pc, index, answer);
+    }).emit('central');
+
+    try {
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      socket.emit('call', index, offer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const urlParams = Object.create(null);
   window.location.search.replace(/^\?/, '').split('&').map(function (pair) {
     return pair.split(/=/).map(decodeURIComponent);
@@ -24,7 +34,9 @@ $(function () {
   });
 
   if ('index' in urlParams) {
-    doCall(parseInt(urlParams.index, 10), io.connect());
+    const index = parseInt(urlParams.index, 10);
+    const socket = io.connect();
+    call(index, socket);
   } else {
     window.location = '/';
   }
--- static/js/camera.js
+++ static/js/camera.js
@@ -8,27 +8,38 @@ $(function () {
     });
   });
 
-  navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true,
-  }).then(function (stream) {
+  const accept = async function (stream, socket, index, offer) {
+    const pc = new RTCPeerConnection();
+    pc.addStream(stream);
+    try {
+      await pc.setRemoteDescription(offer);
+      const answer = await pc.createAnswer();
+      await pc.setLocalDescription(answer);
+      socket.emit('call', index, answer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const listen = function (stream) {
     const socket = io.connect();
-    socket.on('call', function (index, sdp) {
-      const pc = new RTCPeerConnection();
-      var localDescription;
-      pc.setRemoteDescription(sdp).then(function () {
-        return pc.createAnswer();
-      }).then(function (sdp) {
-        localDescription = sdp;
-        return pc.setLocalDescription(sdp);
-      }).then(function () {
-        socket.emit('call', index, localDescription);
-      }).catch(function (error) {
-        console.error(error);
-      });
+    socket.on('call', function (index, offer) {
+      accept(stream, socket, index, offer);
     }).emit('camera');
-    video.prop('srcObject', stream);
-  }).catch(function (error) {
-    console.error(error);
-  });
+  };
+
+  const start = async function () {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+      });
+      video.prop('srcObject', stream);
+      listen(stream);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  start();
 });
